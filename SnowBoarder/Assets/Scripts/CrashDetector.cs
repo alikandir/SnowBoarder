9895b0ea2307ef67e6a7021eaeba5047222d27e4
alikandir <alikandir@gmail.com>
2023-04-07 21:32:34 +0300

    ParticleEffect

--- SnowBoarder/Assets/Scripts/CrashDetector.cs
+++ SnowBoarder/Assets/Scripts/CrashDetector.cs
@@ -6,10 +6,11 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
    [SerializeField] float delayTimeCrash = 0.5f;
+   [SerializeField] ParticleSystem crashEffect;
     void OnTriggerEnter2D(Collider2D other)
     {
         if( other.tag == "Ground")
-        {
+        {  crashEffect.Play();
            Invoke("ReloadScene", delayTimeCrash);
         }
     }
--- SnowBoarder/Assets/Scripts/FinishLine.cs
+++ SnowBoarder/Assets/Scripts/FinishLine.cs
@@ -6,11 +6,14 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float delayTime = 2f;
+    [SerializeField] ParticleSystem finishEffect;
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
+         finishEffect.Play();
         Invoke("ReloadScene", delayTime);
+        
         }
     }
 
